95b712bc841d1c7d00d4f9cac41c272987e5206d
matix <40892703+matix2@users.noreply.github.com>
2022-04-26 19:33:17 -0400

    Moved ApiAvatar button to an AllowAvatarCopying hook.
    Moved ApiAvatar button to a direct AllowAvatarCopying hook.

---
+++ ForceClone/Hooks.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using VRC.Core;
+using MelonLoader;
+
+namespace ForceClone
+{
+    namespace Hooks
+    {
+        public class AllowAvatarCopying
+        {
+            readonly private HarmonyLib.Harmony harmonyInstance;
+
+            public AllowAvatarCopying(HarmonyLib.Harmony harmonyInstance)
+            {
+                this.harmonyInstance = harmonyInstance;
+            }
+
+            private readonly MethodBase originalMethod = typeof(APIUser).GetProperty(nameof(APIUser.allowAvatarCopying)).GetSetMethod();
+            private readonly HarmonyLib.HarmonyMethod hookedMethod = new HarmonyLib.HarmonyMethod(typeof(AllowAvatarCopying).GetMethod(nameof(AllowAvatarCopying.Hook),
+                BindingFlags.NonPublic | BindingFlags.Static));
+
+            public bool isHooked = false;
+
+            public void Initialize()
+            {
+                harmonyInstance.Patch(originalMethod, hookedMethod);
+                isHooked = true;
+            }
+
+            public void Release()
+            {
+                harmonyInstance.Unpatch(originalMethod, hookedMethod.method);
+                isHooked = false;
+            }
+
+            private static void Hook(ref bool __0) => __0 = true;
+        }
+    }
+}
